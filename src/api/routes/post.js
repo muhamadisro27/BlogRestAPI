e6ba336ddeb804296168ef614d3ef2bcfaa799f1
muhamadisro27 <mohammadisro2710@gmail.com>
2022-03-27 20:09:28 +0800

    feat : add route create post

--- src/api/routes/post.js
+++ src/api/routes/post.js
@@ -2,9 +2,10 @@ const express = require("express")
 
 const router = express.Router()
 
-const { getPosts } = require("../controllers/post")
+const { getPosts, createPost } = require("../controllers/post")
 
 
 router.get("/", getPosts)
+router.post("/", createPost)
 
 module.exports = router
