a047128df3f29ef2f06017c38e55a24dd258db91
muhamadisro27 <mohammadisro2710@gmail.com>
2022-03-27 21:28:55 +0800

    feat : setup controller get data & create post

--- src/api/controllers/post.js
+++ src/api/controllers/post.js
@@ -1,12 +1,53 @@
+const db = require("../../config/db")
+const Post = db.post
+
 module.exports = {
    getPosts : async (req,res) => {
+      const post = await Post.findAll()
       res.json({
-         "message" : "get all posts"
+         "message" : "Get all posts",
+         "data" : post,
       })
    },
-   createPost : async (req,res) => {
-      res.json({
-         "message" : "create a post"
-      })
+   createPost : (req,res) => {
+
+      if(!req.body.title) {
+         res.status(400).send({
+            "message" : "Title can not be empty!"
+         })
+      }
+      if(!req.body.description) {
+         res.status(400).send({
+            "message" : "Description can not be empty!"
+         })
+      }
+      if(!req.body.published) {
+         res.status(400).send({
+            "message" : "Published can not be empty!"
+         })
+      }
+
+      const { title , description , published } = req.body
+
+      const post = {
+         title ,
+         description,
+         published
+      }
+
+      Post.create(post).then((data) => {
+         res.json({
+            "status" : "success",
+            "message" : "Success create a new post!",
+            data
+         },201)
+
+      }).catch((err) => {
+         res.status(500).send({
+            message : err.message || "Some error occured while created post"
+         })
+      });
+
+     
    }
 }
