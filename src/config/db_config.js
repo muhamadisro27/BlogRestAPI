5429160bf91a6144526e1727910da6be6092f3b2
muhamadisro27 <mohammadisro2710@gmail.com>
2022-03-27 21:28:14 +0800

    feat : setup db

--- src/config/db_config.js
+++ src/config/db_config.js
@@ -1,3 +1,13 @@
 module.exports = {
    HOST : "localhost",
+   USER : "root",
+   PASSWORD : "",
+   DB : "blogrestapi",
+   dialect : "mysql",
+   pool : {
+      min : 0,
+      max : 5,
+      acquire: 30000,
+      idle: 10000,
+   }
 }
