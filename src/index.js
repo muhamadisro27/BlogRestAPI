1b5857ea078c175896b6ed37edd474c7d50d8b73
muhamadisro27 <mohammadisro2710@gmail.com>
2022-03-27 20:10:07 +0800

    feat : setup cors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const PORT = process.env.PORT || 4000
+const cors = require("cors")
+
+const whiteList = [
+   'https://localhost:4001'
+]
+
+let corsOptions = {
+   origin : (origin,callback) => {
+      if (whiteList.indexOf(origin) !== -1 || !origin) {
+         callback(null,true)
+      } else {
+         callback(new Error("Not Allowed by CORS"))
+      }
+   }
+}
+
 
 // Import routes
 const postRoutes = require("./api/routes/post")
@@ -10,15 +26,16 @@ const postRoutes = require("./api/routes/post")
 // middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-
+app.use(cors(corsOptions))
 
 // Routes
+app.use("/post", postRoutes)
 app.use("/", (req,res) => {
    res.json({
       "message" : "index"
    })
 })
-app.use("/post", postRoutes)
+
 
 
 
