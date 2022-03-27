f94380b2682597a48006cddab1cf52b798bc448f
muhamadisro27 <mohammadisro2710@gmail.com>
2022-03-28 00:05:12 +0800

    feat : build route delete post

--- src/api/routes/post.js
+++ src/api/routes/post.js
@@ -2,12 +2,13 @@ const express = require("express")
 
 const router = express.Router()
 
-const { getPosts, createPost , getPost, deletePost} = require("../controllers/post")
+const { getPosts, createPost , getPost, updatePost, deletePost} = require("../controllers/post")
 
 
 router.get("/", getPosts)
 router.post("/", createPost)
 router.get("/:id", getPost)
+router.put("/:id", updatePost)
 router.delete("/:id", deletePost)
 
 module.exports = router
