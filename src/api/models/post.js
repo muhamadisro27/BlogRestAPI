0d79887927df72f5d933f05ed2de9fa99431eeba
muhamadisro27 <mohammadisro2710@gmail.com>
2022-03-28 18:46:52 +0800

    feat : setup model post and relation

--- src/api/models/post.js
+++ src/api/models/post.js
@@ -1,20 +1,28 @@
 module.exports = (sequelize,Sequelize) => {
-   const Post = sequelize.define("post", {
+   const Post = sequelize.define("posts", {
+      uuid : {
+         type : Sequelize.UUID,
+         allowNull : false,
+      },
       title : {
          type : Sequelize.STRING,
-         allowNul : false,
+         allowNull : false,
          trim : true,
          len : [10,255]
       },
+      slug : {
+         type : Sequelize.STRING,
+         allowNull : false,
+      },
       description : {
          type : Sequelize.TEXT,
-         allowNul : false,
+         allowNull : false,
          trim : true,
          len : [10,255]
       },
       published : {
          type : Sequelize.BOOLEAN,
-         allowNul : false,
+         allowNull : false,
       }
    })
 
