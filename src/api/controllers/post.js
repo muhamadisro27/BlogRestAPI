5701954bd4635bae99059e3a39453e5ef9bc3281
muhamadisro27 <mohammadisro2710@gmail.com>
2022-03-28 10:24:52 +0800

    feat : refactor build service read,update,delete,publish

--- src/api/controllers/post.js
+++ src/api/controllers/post.js
@@ -16,59 +16,31 @@ module.exports = {
    },
    createPost : async (req,res) => {
     try {
-         if(!req.body.title) {
-            res.status(400).send({
-               "message" : "Title can not be empty!"
-            })
-         }
-         if(!req.body.description) {
-            res.status(400).send({
-               "message" : "Description can not be empty!"
-            })
-         }
-         if(!req.body.published) {
-            res.status(400).send({
-               "message" : "Published can not be empty!"
-            })
-         }
+       const service = new PostService(req)
+       const createPost = service.createPost()
+      
+       res.send({
+          message : "Success create a new post!",
+          data : createPost,
+       })
 
-         const { title , description , published } = req.body
-
-         const post = {
-            title ,
-            description,
-            published : published ? published : false
-         }
-
-         await Post.create(post).then((data) => {
-            res.json({
-               "status" : "success",
-               "message" : "Success create a new post!",
-               data
-            },201)
-
-         }).catch((err) => {
-            res.status(500).send({
-               message : err.message || "Some error occured while created post"
-            })
-         });
     } catch (err) {
-       console.error(err)
+       res.status(400).send({
+          message : err.message || `Error on occurred!`
+       })
     }
 
      
    },
    getPost : async (req,res) => {
       try {
-         const {id} = req.params
-
-         const post = await Post.findOne({where : {
-            id
-         }})
+         const service = new PostService(req)
+         const post = await service.getPost()
    
          res.json({
             data : post
          }).send()
+
       } catch (err) {
          res.send(err)
       }
@@ -76,80 +48,45 @@ module.exports = {
 
    },
    updatePost : async (req,res) => {
-      const {id} = req.params
+      try {
+         const service = new PostService(req)
+         const updatePost = await service.updatePost()
 
-      const {title,description, published} = req.body
-
-      if(!title) {
-         res.status(400).send({
-            message : "Title can not be empty!"
-         })
-      }
-      if(!description) {
-         res.status(400).send({
-            message : "Title can not be empty!"
-         })
-      }
-      if(!published) {
-         res.status(400).send({
-            message : "Title can not be empty!"
-         })
-      }
-
-      const post = {
-         title,description, published
-      }
-
-      await Post.update(post, {
-         where : {id}
-      }).then(() => {
          res.send({
-            status : "success",
-            message : `Post with id ${id} has been updated!`,
-         },200)
-      }).catch((err) => {
-         res.status(500).send({
-            status : "failed",
-            message : err.message || `Post with id ${id} fail to update!`
+            message : "Success update data",
          })
-      })
+      } catch (error) {
+         res.status(500).send({
+            message : error.message || `Error on occurred!`
+         })
+      }
    },
    updatePublishPost : async (req,res) => {
-      const {id} = req.params
+      try {
+         const service = new PostService(req)
+         const updatePublishPost = await service.updatePublishPost()
 
-      await Post.update({published : req.body.published},{where : {id}}).then(() => {
          res.send({
-            message : `Data with id ${id} has been publish!`
-         }).catch((err) => {
-            res.status(500).send({
-               message : err.message || `Data with id ${id} fail to publish!`
-            })
+            message : `Data has been publish!`
          })
-      })
+      } catch (error) {
+         res.status(500).send({
+            message : err.message 
+         })
+      }
    },
    deletePost : async (req,res) => {
-         const {id} = req.params
-
-         await Post.destroy({
-            where : {
-               id
-           }
-         }).then((result) => {
-            if (result === 1) {
-               res.send({
-                  message : "Post was deleted succesfully"
-               })
-            } else {
-               res.send({
-                  message : "Can not deleted post with id = " + id
-               })
-            }
-         }).catch((err) => {
-            res.status(500).send({
-               message : err.message || `Can not delete post with id = ${id}`
-            })
+      try {
+         const service = new PostService(req)
+         const deletePost = service.deletePost()
+ 
+         res.send({
+            message : `Success delete data!`
          })
-       
-
+      } catch (err) {
+         res.status(500).send({
+            message : err.message || `Error has on occurred!`
+         })
+      }
    }
 }
--- src/api/services/post.js
+++ src/api/services/post.js
@@ -12,9 +12,99 @@ class PostService {
 
       return post
    }
+   createPost = async() => {
+      if(!this.body.title) {
+         res.status(400).send({
+            "message" : "Title can not be empty!"
+         })
+      }
+      if(!this.body.description) {
+         res.status(400).send({
+            "message" : "Description can not be empty!"
+         })
+      }
+      if(!this.body.published) {
+         res.status(400).send({
+            "message" : "Published can not be empty!"
+         })
+      }
 
-   
+      const { title , description , published } = this.body
 
+      const post = {
+         title ,
+         description,
+         published : published ? published : false
+      }
+
+      const createPost = await Post.create(post)
+
+      return createPost
+   }
+   getPost = async() => {
+      const {id} = this.params
+
+      const post = await Post.findOne({where : {
+         id
+      }})
+
+      return post
+   }
+   updatePost = async() => {
+      const {id} = this.params
+
+      const {title,description, published} = this.body
+
+      if(!title) {
+         res.status(400).send({
+            message : "Title can not be empty!"
+         })
+      }
+      if(!description) {
+         res.status(400).send({
+            message : "Title can not be empty!"
+         })
+      }
+      if(!published) {
+         res.status(400).send({
+            message : "Title can not be empty!"
+         })
+      }
+
+      const post = {
+         title,description, published
+      }
+
+      const updatePost = await Post.update(post, {
+         where : {id}
+      })
+
+      return updatePost
+   }
+   updatePublishPost = async() => {
+      const {id} = this.params
+
+      const post = await Post.update({published : this.body.published},{where : {id}})
+
+      return post
+   }
+   deletePost = async() => {
+      const {id} = this.params
+
+      if (!id === 1) {
+         res.status(404).send({
+            message : `Data with id = ${id} not found`
+         })
+      }
+
+      const post = await Post.destroy({
+         where : {
+            id
+        }
+      })
+
+      return post
+   }
 }
 
 
